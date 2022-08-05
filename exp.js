012ebadc6b50aae679ff2bea80d87833f3a80e02
leowilson <leobca2018@gmail.com>
2022-08-05 19:36:56 +0530

    add-export-file.

---
+++ exp.js
@@ -0,0 +1,65 @@
+export var name = 'Hello';
+export var sample = [2, 3, 4, 5, 5];
+export var hostels = [
+    {
+        id: 1,
+        name: 'Zolo Dune',
+        location: 'Chennai',
+        food_availability: 'Yes'
+    },
+    {
+        id: 2,
+        name: 'Erbil House',
+        location: 'Guduvanchery',
+        food_availability: 'No'
+    },
+    {
+        id: 3,
+        name: 'Fun PG',
+        location: 'Vandalur',
+        food_availability: 'Yes'
+    },
+    {
+        id: 4,
+        name: 'Sai Atcharaa Boys Hostel',
+        location: 'Chennai',
+        food_availability: 'Yes'
+    },
+    {
+        id: 5,
+        name: 'Sai Atcharaa Boys Hostel',
+        location: 'Guindy',
+        food_availability: 'No'
+    },
+    {
+        id: 6,
+        name: 'Zolo Belford',
+        location: 'Velachery',
+        food_availability: 'Yes'
+    },
+    {
+        id: 7,
+        name: 'Sai Atcharaa Boys Hostel',
+        location: 'Guindy',
+        food_availability: 'No'
+    },
+    {
+        id: 8,
+        name: 'Pudur Lucky Hostel',
+        location: 'Chepak',
+        food_availability: 'Yes'
+    },
+    {
+        id: 9,
+        name: 'Royal Comfor Mansion',
+        location: 'Vadapalani',
+        food_availability: 'No'
+    },
+    {
+        id: 10,
+        name: 'SR Boys Hostel',
+        location: 'Kattankulathur',
+        food_availability: 'Yes'
+    }
+];
+
